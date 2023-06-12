822caeb0d08490cf8cfc82e6b49f067956a520e1
Denis mulwa <marshmellowking99@gmail.com>
2023-06-12 12:47:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser;
+currentUser = 'Denis';
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const upperCaseCurrentUser = currentUser.toUpperCase();
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}\!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}\!`;
+let shortGreeting = `Welcome, ${firstInitial}\!`
